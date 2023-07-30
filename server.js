9e15d6043bb6cb34d3687da024299b1bf416d09e
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-30 14:26:55 -0300

    removed force in sync, getting major errors

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
@@ -12,6 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync().then(() => {
   console.log(`App listening on port ${PORT}!`);
 });
