fc64b4c3219b05edc29d441be5ac3fd53ef95498
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-30 20:36:33 -0300

    added requirements to product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,18 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+    }
   },
   {
     sequelize,
@@ -18,18 +30,3 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
-
-/**
-ProductTag
-id
-Integer
-Doesn't allow null values
-Set as primary key
-Uses auto increment
-product_id
-Integer
-References the product model's id
-tag_id
-Integer
-References the tag model's id
- */
