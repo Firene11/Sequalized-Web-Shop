936776ba7d47586d6de6a191b661533af7b290c5
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-30 14:45:30 -0300

    updated schema with tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,34 @@ CREATE DATABASE ecommerce_db;
 
 USE ecommerce_db;
 
-drop table if exists Category;
+DROP TABLE IF EXISTS Category;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Product_Tag;
+DROP TABLE IF EXISTS Tag;
 
-create table Category (
+CREATE TABLE Category (
     id int not null auto_increment primary key,
     category_name varchar(30) not null
+);
+
+CREATE TABLE Product (
+   id int not null auto_increment primary key,
+   product_name varchar(30) not null,
+   price decimal not null,
+   stock int not null default 10,
+   category_id int,
+   foreign key (category_id) references Category(id)
+);
+
+CREATE TABLE Tag (
+    id int not null auto_increment primary key,
+    tag_name varchar(30)
+);
+
+CREATE TABLE Product_Tag (
+    id int not null auto_increment primary key,
+    product_id int,
+    foreign key (product_id) references Product(id),
+    tag_id int,
+    foreign key (tag_id) references Tag(id)
 );
