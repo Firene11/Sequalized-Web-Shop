c2685476f24edd0cbfb6d1682a18211917699502
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-30 20:40:22 -0300

    added product belongs to category

--- models/index.js
+++ models/index.js
@@ -5,6 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Cetgory, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
 
@@ -18,3 +21,16 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+/*
+Product belongs to Category, as a category can have multiple products but a product can only belong to one 
+category.
+
+Category has many Product models.
+
+Product belongs to many Tag models. Using the ProductTag through model, allow products to have multiple 
+tags and tags to have many products.
+
+Tag belongs to many Product models.
+*/
