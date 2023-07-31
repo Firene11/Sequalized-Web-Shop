b67087e2f247b9b677555b96bcaf4de906925fa6
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-31 11:12:34 -0300

    changed sync force to true

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('/', (req, res) => {
 });
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Now listening to PORT:!${PORT}`));
 });
